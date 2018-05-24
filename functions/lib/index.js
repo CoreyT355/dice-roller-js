d3e73639676f51da3a572a512f6380d9efaa7e1f
Corey <corey.a.tess@gmail.com>
2018-05-24 19:43:18 -0400

    It's Alive!
    Working in Firebase now.

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -12,20 +12,19 @@ const functions = require("firebase-functions");
 const Slack = require("./models/slack.model");
 const roll_controller_1 = require("./logic/roll.controller");
 exports.rollDice = functions.https.onRequest((req, res) => __awaiter(this, void 0, void 0, function* () {
+    console.log(`Token: ${req.body.token}`);
+    console.log(`Text: ${req.body.text}`);
     const slackRes = new Slack.Response();
     const allowedTokens = [
-        "ZPLkPLJKwgo3oSVldYcEhtHn",
-        "nzuisV6QeBIWv83VEiJV8E1m",
-        "QqCPp9777BuQAMzt3xMZ512K",
         "uTzgipm2SXe415vtiVH4gbUz"
     ];
-    const authToken = req.query.token;
+    const authToken = req.body.token;
     if (!allowedTokens.find(token => { return token === authToken; })) {
         slackRes.response_type = 'ephemeral';
         slackRes.text = `Auth Failed: broken token`;
         res.status(418).send(slackRes);
     }
-    const rollParams = roll_controller_1.RollController.SplitWhatToRoll(req.query.whatToRoll);
+    const rollParams = roll_controller_1.RollController.SplitWhatToRoll(req.body.text);
     const result = roll_controller_1.RollController.rollDemBones(rollParams.numberOfDice, rollParams.typeOfDice, rollParams.diceModifier);
     slackRes.response_type = 'in_channel';
     slackRes.text = `Your result is....${result}`;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,23 +2,20 @@ import * as functions from 'firebase-functions';
 import * as Slack from './models/slack.model';
 import { RollController } from './logic/roll.controller';
 export const rollDice = functions.https.onRequest(async (req, res) => {
+    const requestBody = req.body;
     const slackRes = new Slack.Response();
     const allowedTokens = [
-        "ZPLkPLJKwgo3oSVldYcEhtHn",
-        "nzuisV6QeBIWv83VEiJV8E1m",
-        "QqCPp9777BuQAMzt3xMZ512K",
         "uTzgipm2SXe415vtiVH4gbUz"
     ];     
-    const authToken = req.query.token;
+    const authToken = requestBody.token;
     if (!allowedTokens.find(token => { return token === authToken; })) {
         slackRes.response_type = 'ephemeral';
         slackRes.text = `Auth Failed: broken token`;
         res.status(418).send(slackRes);
     }
-    const rollParams = RollController.SplitWhatToRoll(req.query.whatToRoll);
+    const rollParams = RollController.SplitWhatToRoll(requestBody.text);
     const result = RollController.rollDemBones(rollParams.numberOfDice, rollParams.typeOfDice, rollParams.diceModifier);
     slackRes.response_type = 'in_channel';
     slackRes.text = `Your result is....${result}`;
     res.status(200).send(slackRes);
 });
-
