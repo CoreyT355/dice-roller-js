4990246ac85ab0e71dc3c8029e4abfa3083e3487
Corey <corey.a.tess@gmail.com>
2018-05-29 11:36:21 -0400

    Improved Randomizer
    More random random numbers.
    closes #4

--- functions/lib/logic/number-generator.controller.js
+++ functions/lib/logic/number-generator.controller.js
@@ -1,8 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const crypto = require("crypto");
 class Generator {
     static GetNumberBetween(min, max) {
-        return Math.floor(Math.random() * max) + min;
+        //return Math.floor(Math.random() * max) + min;
+        return Math.floor(this.GetCryptoRando() * (max - min) + min);
+    }
+    static GetCryptoRando() {
+        const buffer = crypto.randomBytes(8);
+        return this.IntToFloat(parseInt(buffer.toString('hex'), 16));
+    }
+    static IntToFloat(integer) {
+        return integer / Math.pow(2, 64);
     }
 }
 exports.Generator = Generator;
--- functions/src/logic/number-generator.controller.ts
+++ functions/src/logic/number-generator.controller.ts
@@ -1,5 +1,15 @@
+import * as crypto from "crypto";
+
 export class Generator {
     public static GetNumberBetween(min: number, max: number): number {
-        return Math.floor(Math.random() * max) + min;
+        //return Math.floor(Math.random() * max) + min;
+        return Math.floor(this.GetCryptoRando() * (max - min) + min);
+    }
+    private static GetCryptoRando(): number {
+        const buffer = crypto.randomBytes(8);
+        return this.IntToFloat(parseInt(buffer.toString('hex'), 16));
+    }
+    private static IntToFloat(integer): number {
+        return integer / Math.pow(2, 64);
     }
 }
